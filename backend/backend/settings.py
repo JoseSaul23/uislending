7c47f4acd39cc191bd0f690ad074f5ced76aec87
JoseSaul123 <josevega2398@gmail.com>
2020-02-23 17:10:37 -0500

    celery y redis

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -184,6 +184,15 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, "media")
 
-CELERY_BROKER_URL = 'redis://localhost'
+# REDIS CONFIG
+REDIS_URL = os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379')
+BROKER_CONNECTION_MAX_RETRIES = os.environ.get('BROKER_CONNECTION_MAX_RETRIES', None)
+BROKER_POOL_LIMIT = os.environ.get('BROKER_POOL_LIMIT', None)
+
+# CELERY CONFIG
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', REDIS_URL)
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', REDIS_URL)
+CELERY_REDIS_MAX_CONNECTIONS = os.environ.get('CELERY_REDIS_MAX_CONNECTIONS', 5)
+CELERYD_CONCURRENCY = os.environ.get('CELERYD_CONCURRENCY', 1)
 
 DATABASES['default'] = dj_database_url.config(ssl_require=False)
