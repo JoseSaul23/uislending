cb896bf615ed8af8924a03e0dd7bdda90b671ece
JoseSaul123 <josevega2398@gmail.com>
2020-02-24 09:19:53 -0500

    Statics en Amazon s3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -218,7 +218,7 @@ if USE_S3:
     # s3 static settings
     STATIC_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-    STATICFILES_STORAGE = 'hello_django.storage_backends.StaticStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
